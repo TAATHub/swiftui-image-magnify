06c018b661534adb92c58a75888fd114933e96cb
TAAT <20purpleblue10@gmail.com>
2023-05-07 13:31:04 +0900

    [add] コメントを追加

--- ImageMagnify/ImageMagnificationModifier.swift
+++ ImageMagnify/ImageMagnificationModifier.swift
@@ -13,25 +13,31 @@ struct ImageMagnificationModifier: ViewModifier {
         self.contentSize = contentSize
         self.aspectRatio = aspectRatio
     }
-
+    
     var magnification: some Gesture {
         MagnificationGesture()
             .onChanged { value in
+                // 前回の拡大率に対する今回の拡大率の割合
                 let changeRate = value / lastMagnificationValue
-                lastMagnificationValue = value
                 
                 DispatchQueue.main.async {
+                    // 前回からの拡大率の変化分を考慮した現在のスケールを計算
                     currentScale *= changeRate
+                    // 最小・最大スケールの範囲内に収める
                     currentScale = min(max(minScale, currentScale), maxScale)
                 }
+                
+                lastMagnificationValue = value
             }
             .onEnded { value in
+                // ジェスチャー開始時は1.0から始まるため、ジェスチャー終了時に1.0に戻す
                 lastMagnificationValue = 1.0
             }
     }
     
     var doubleTap: some Gesture {
         TapGesture(count: 2).onEnded {
+            // 最小・最大スケールを切り替える
             currentScale = currentScale < maxScale ? maxScale : minScale
         }
     }
@@ -42,7 +48,9 @@ struct ImageMagnificationModifier: ViewModifier {
                 .scaleEffect(currentScale)
                 .gesture(magnification)
                 .simultaneousGesture(doubleTap)
-                .frame(width: contentSize.width * currentScale, height: contentSize.width / aspectRatio * currentScale, alignment: .center)
+                .frame(width: contentSize.width * currentScale,
+                       height: contentSize.width / aspectRatio * currentScale,
+                       alignment: .center)
         }
     }
 }
